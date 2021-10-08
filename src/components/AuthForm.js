42b4e69af68f50f24f8e73ba4777431b73a22577
Ivan Ushmorov <ivan.ushmorov@zeiss.com>
2021-10-08 23:05:27 +0200

    styled the authform

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -5,11 +5,14 @@ import supabase from '../lib/supabase'
 
 const AuthForm = () => {
     const [email, setEmail] = useState();
+    const [mailSent, setMailSent] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
         const { error } = await supabase.auth.signIn({ email })
+
+        setMailSent(true);
 
         console.log({ error })
     }
@@ -17,25 +20,37 @@ const AuthForm = () => {
     return (
         <div className={"auth-form"}>
             <h1>trackrrr</h1>
-            <h2>Yet another time tracking tool</h2>
+            <h2 className={"mb-m"}>Yet another time tracking tool</h2>
 
-            <p>
-                Fill in your email, we'll send you a magic link.
-            </p>
+            {!mailSent && 
+            (<>
+                <p>
+                    Fill in your email, we'll send you a magic link.
+                </p>
 
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    placeholder="Your email address"
-                    onChange={e => setEmail(e.target.value)}
-                />
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="email"
+                        placeholder="Your email address"
+                        onChange={e => setEmail(e.target.value)}
+                    />
 
-                <button
-                    type="submit"
-                >
-                    Let's go!
-                </button>
-            </form>
+                    <button
+                        type="submit"
+                        className={"turquoise-flow"}
+                    >
+                        Let's go!
+                    </button>
+                </form>
+            </>
+            )}
+
+            {mailSent && 
+            (
+                <p>
+                    Mail has been sent.<br/>Please check your mails and follow the link.
+                </p>   
+            )}
         </div>
     )
 }
